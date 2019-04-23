02f8cec97e67cb38946acacdfc21ea9ce7926f07
beg1shev <begishev1998@mail.ru>
2019-04-24 02:55:09 +0300

    Insert queries added

--- project_delivery.py
+++ project_delivery.py
@@ -37,3 +37,75 @@ patient_point = r.point(coords[0], coords[1])
 pprint(r.db("HMS").table("Ambulances").get_nearest(patient_point, index='coordinates', max_results=2, unit='km').run(conn))
 
 colors.Color.from_rgb()
+
+#INSERT ONE ENTRY
+r.db("HMS").table("Ambulances").insert({
+    'primary_id': 77,
+    'driver_id': 50,
+    'doctor_id': 36,
+    'paramedic_ids': [55, 73, 125],
+    'coordinates': r.point(55.752258, 48.744576),
+    'additional_data': {"the fastest ambulance in hospital"}
+        }).run(conn)
+pprint(r.db("HMS").table("Ambulances").run(conn))
+
+
+#INSERT MULTIPLE ENTRIES
+patient_id = 10000
+illness_history_id = 10001
+form_id_1 = 10002
+form_id_2 = 10003
+doctor_id = 10004
+
+
+r.db("HMS").table("Patients").insert({
+    'primary_id': patient_id,
+    'login': 'john.terry',
+    'password': 'password12',
+    'name': 'John Terry',
+    'date_of_birth': '1980',
+    'sex': 'male',
+    'SSN_ID': 4735501961098410,
+    'telephone': 88005553535,
+    'home_address': "Innopolis, Universitetskaya, 1, 306",
+    'coordinates': r.point(55.43423, 47.76545),
+    'illness_history_head_id': illness_history_id,
+    'additional_data': {"problem patient"}
+}).run(conn)
+pprint(r.db("HMS").table("Patients").filter({'name': 'John Terry'}).run(conn))
+
+
+r.db("HMS").table("IllnessHistories").insert({
+            'primary_id': illness_history_id,
+            'owner_id': patient_id,
+            'forms_ids': [form_id_1, form_id_2],
+            'next_id': '',
+            'additional_data': {"history of problem patient"}
+        }).run(conn)
+pprint(r.db("HMS").table("IllnessHistories").filter({'owner_id': patient_id}).run(conn))
+
+
+r.db("HMS").table('IllnessForms').insert({
+                'primary_id': form_id_1,
+                'form_type': "069-uf",
+                'doctor_id': doctor_id,
+                'patient_id': patient_id,
+                'procedure_type': "bandaging",
+                'additional_data': {
+                    'operation_type': 'operation on arm',
+                    'result': 'success'
+                }
+            }).run(conn)
+
+r.db("HMS").table('IllnessForms').insert({
+                'primary_id': form_id_2,
+                'form_type': "183-op",
+                'doctor_id': doctor_id + 1,
+                'patient_id': patient_id,
+                'procedure_type': "electrodiagnostics",
+                'additional_data': {
+                    'operation_type': 'operation on arm',
+                    'result': 'success'
+                }
+            }).run(conn)
+pprint(r.db("HMS").table("IllnessForms").filter({'patient_id' : patient_id}).run(conn))
